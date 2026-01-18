79900ee5d62a6c6a6ae2049d5cfbf66d3e183cb2
zDefcon <antholozadarequelme@gmail.com>
2026-01-18 06:36:24 -0500

    Agrega método para ingresar calificaciones en EjercicioCalificaciones
    Implementa la función IngresarCalificaciones que permite al usuario ingresar cinco calificaciones finales, validando que sean números entre 0 y 10. Se incluye un mensaje de bienvenida y se asegura que los datos ingresados sean correctos antes de proceder.

--- src/Logica/EjercicioCalificaciones.java
+++ src/Logica/EjercicioCalificaciones.java
@@ -1,4 +1,37 @@
 ﻿package Logica;
+import java.util.Scanner;
 
 public class EjercicioCalificaciones {
+    private void IngresarCalificaciones(){
+        Scanner input = new Scanner(System.in);
+        double[] notasF = new double[5];
+        int e = 0;
+        System.out.println("---------------------------------------");
+        System.out.println("Ingrese Las 5 Calificaciones Finales");
+        for (int i = 0; i < notasF.length; i++) {
+            System.out.print("Ingrese Nota["+i+"]: ");
+            notasF[i] = input.hasNextDouble() ? input.nextDouble() : (e = 1) ;
+            if(e == 1){
+                System.out.println("Error: Valor Invalido, Ingrese Un Numero");
+                e = 0;
+                i--;
+            }
+            if(notasF[i] < 0 || notasF[i] > 10){
+                System.out.println("Error: La Nota Debe Estar Entre 0 y 10");
+                i--;
+            }
+        }
+        System.out.println("Notas Ingresadas Correctamente");
+        System.out.println("---------------------------------------");
+
+    }
+    public void main(String[] args) {
+        System.out.println("---------------------------------------");
+        System.out.println("Bienvenido Al Sistema De Calificaciones");
+        IngresarCalificaciones();
+
+    }
+
+
+
 }
