6dc00a0be1fef1eecd7f0a63a7b094eb288f630d
zDefcon <antholozadarequelme@gmail.com>
2026-01-18 06:57:13 -0500

    Agrega método para calcular el promedio del curso
    Se modifica el método IngresarCalificaciones para recibir un arreglo de calificaciones como parámetro y se agrega el método calcularPromedioCurso que calcula y muestra el promedio de las calificaciones ingresadas. Se incluye un mensaje que indica si el promedio es bueno o malo.

--- src/Logica/EjercicioCalificaciones.java
+++ src/Logica/EjercicioCalificaciones.java
@@ -2,9 +2,8 @@ package Logica;
 import java.util.Scanner;
 
 public class EjercicioCalificaciones {
-    private void IngresarCalificaciones(){
+    private double[] IngresarCalificaciones(double[] notasF){
         Scanner input = new Scanner(System.in);
-        double[] notasF = new double[5];
         int e = 0;
         System.out.println("---------------------------------------");
         System.out.println("Ingrese Las 5 Calificaciones Finales");
@@ -25,12 +24,31 @@ public class EjercicioCalificaciones {
         }
         System.out.println("-----Notas Ingresadas Correctamente----");
         System.out.println("---------------------------------------");
+        return notasF;
+    }
 
+    private double calcularPromedioCurso(double[] notasF){
+        double suma = 0;
+        for (int i = 0; i < notasF.length; i++) {
+            suma = suma + notasF[i];
+        }
+        double promedio = suma / notasF.length;
+        return promedio;
     }
     public void main(String[] args) {
+        double[] notasF = new double[5];
         System.out.println("---------------------------------------");
         System.out.println("Bienvenido Al Sistema De Calificaciones");
-        IngresarCalificaciones();
+        IngresarCalificaciones(notasF);
+        System.out.println("---------------------------------------");
+        double promedio = calcularPromedioCurso(notasF);
+        System.out.println("El Promedio Del Curso Es: " + String.format("%.2f", promedio));
+        if(promedio >= 7){
+            System.out.println("El Promedio Del Curso Es Bueno");
+        }else{
+            System.out.println("El Promedio Del Curso Es Malo, Necesitan Mejorar");
+        }
+        System.out.println("---------------------------------------");
 
     }
 
