63f24db961c7bda055853653f626134627635c83
zDefcon21 <antholozadarequelme@gmail.com>
2026-05-12 18:47:55 -0500

    Agrega clase para invertir cadenas de texto
    Se implementa la clase InvertirCadenaStrings que permite al usuario ingresar un texto y obtener su versión invertida. La funcionalidad incluye la conversión del texto a un arreglo de caracteres y el intercambio de posiciones para lograr la inversión.

---
+++ src/Logica/InvertirCadenaStrings.java
@@ -0,0 +1,34 @@
+package Logica;
+
+import java.util.Scanner;
+
+public class InvertirCadenaStrings {
+    public static void main(String[] args) {
+        long start = System.nanoTime();
+        Scanner input = new Scanner(System.in);
+        String texto = "", invertido = "";
+        System.out.println("----------Bienvenido--------");
+        System.out.print("Ingrese El Texto a Invertir: ");
+        texto = input.nextLine();
+        System.out.println("----------------------------");
+        invertido = invertirCadena(texto);
+        System.out.println("Cadena Original: " + texto);
+        System.out.println("Cadena Invertida: " + invertido);
+        System.out.println("-----------------------------");
+        long end = System.nanoTime();
+    }
+    public static String invertirCadena(String original){
+        char[] arr = original.toCharArray(); // convierte el String en un array de char
+        int izq = 0; // puntero izquierda
+        int der = arr.length - 1; // puntero derecha
+        while(izq < der){
+            // intercambios y acercar los punteros al centro
+            char temporal = arr[izq];
+            arr[izq] = arr[der];
+            arr[der] = temporal;
+            izq = izq + 1;
+            der = der - 1;
+        }
+        return new String(arr);
+    }
+}
